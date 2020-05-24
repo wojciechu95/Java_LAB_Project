514a74be07a00214a5a1448d54d17cf555c54e21
Wojciech <wojtek.piwowarski1@gmail.com>
2020-05-24 17:28:49 +0200

    API Działa

--- src/main/java/com/webencyclop/demo/controller/ApiController.java
+++ src/main/java/com/webencyclop/demo/controller/ApiController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pwallet")
@@ -19,11 +20,15 @@ public class ApiController {
     @Autowired
     private ITodoService todoService;
 
-    @RequestMapping(path="/getall", method = RequestMethod.GET, produces = "application/json")
-    public List<Todo> getAllTodo(ModelMap model){
+    @RequestMapping(path="/getall", method = RequestMethod.GET, produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
+    public Map<String, List<Todo>> getAllTodo(ModelMap model){
         String name = getLoggedInUserName(model);
 
-        return todoService.getTodosByUser(name);
+        Map<String, List<Todo>> assoc = new HashMap<String, List<Todo>>();
+
+        assoc.put("data", todoService.getTodosByUser(name));
+
+        return assoc;
     }
 
     private String getLoggedInUserName(ModelMap model) {
@@ -35,4 +40,41 @@ public class ApiController {
 
         return principal.toString();
     }
+
+    @DeleteMapping("/delete")
+    public Map<String, Boolean> delete(@RequestParam Long id) {
+
+        todoService.deleteTodo(id);
+
+        Map<String, Boolean> assoc = new HashMap<String, Boolean>();
+
+        assoc.put("success", true);
+
+        return assoc;
+    }
+
+
+//    @RequestMapping(value = "/delete-todo", method = RequestMethod.DELETE)
+//    public String deleteTodo(@RequestParam long id) {
+//        todoService.deleteTodo(id);
+//
+//        return "redirect:/pwallet";
+//    }
+
+//
+//
+//    // GET: Wallet/Delete/5
+//        [HttpDelete]
+//    public async Task<IActionResult> Delete(int id)
+//    {
+//        var PasswdModelFromDb = await _db.PWallet.FirstOrDefaultAsync(u => u.ID == id);
+//        if (PasswdModelFromDb == null)
+//        {
+//            return Json(new { success = false, message = "Błąd w trakcie usuwania" });
+//        }
+//        _db.PWallet.Remove(PasswdModelFromDb);
+//        await _db.SaveChangesAsync();
+//        return Json(new { success = true, message = "Usunieto z Powodzeniem" });
+//    }
+
 }
--- src/main/resources/static/js/WalletTable.js
+++ src/main/resources/static/js/WalletTable.js
@@ -1,24 +1,71 @@
-﻿var dataTable;
+﻿// const swalWithBootstrapButtons = swal.mixin({
+//     customClass: {
+//         confirmButton: 'btn btn-success',
+//         cancelButton: 'btn btn-danger'
+//     },
+//     buttonsStyling: false
+// })
+
+let dataTable;
 
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    var id = 0;
-
+    let isD = false;
     dataTable = $('#DT_load').DataTable({
         "ajax": {
             "url": "pwallet/getall",
             "type": "GET",
-            "dataType": "json"
+            "datatype": "json",
         },
         "columns": [
-            { "isDone": "isDone", "width": "10%" },
-            { "userName": "userName", "width": "10%" },
-            { "description": "description", "width": "30%"},
-            { "targetDate": "targetDate", "width": "30%"},
-            { "id": "id", "width": "20%"}
+            {
+                data: "isDone",
+                "render": function (data) {
+                    let td = data
+                        ? `<td class="text-center"><i class="text-success h3 fas fa-clipboard-check"></i></td>`
+                        : `<td class="text-center"><i class="text-warning h3 fas fa-clipboard-check"></i></td>`;
+                    isD = data;
+                    return td;
+                }, "width": "15%"
+            },
+            {data: "userName", "width": "10%"},
+            {
+                data: "description",
+                "render": function (data) {
+                    let td = !isD
+                        ? `<td class="text-center">${data}</td>`
+                        : `<td class="text-center"><del>${data}</del></td>`;
+                    return td;
+                }, "width": "30%"
+            },
+            {
+                data: "targetDate",
+                "render": function (data) {
+                    let td = !isD
+                        ? `<td class="text-center">${data}</td>`
+                        : `<td class="text-center"><del>${data}</del></td>`;
+                    return td;
+                }, "width": "20%"
+            },
+            {
+                data: "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/pwallet/update-todo?id=${data}" class='btn btn-success text-white'
+                            style='cursor:pointer;'>
+                            Update
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer;'
+                            onclick=Delete('/pwallet/delete?id=${data}')>
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "25%"
+            }
         ],
         "language": {
             "emptyTable": "Nie znaleziono danych"
@@ -28,5 +75,45 @@ function loadDataTable() {
 }
 
 function Delete(url) {
-
+    swal.fire({
+        title: "Czy jesteś pewien?",
+        text: "Po usunięciu nie będzie można odzyskać",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#40be2d',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Nie',
+        confirmButtonText: 'Tak',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.value) {
+            if (result.value) {
+                $.ajax({
+                    type: "DELETE",
+                    url: url,
+                    success: function (data) {
+                        if (data.success) {
+                            swal.fire(
+                                'Usunięto',
+                                'Element został usunięty.',
+                                'success'
+                            )
+                            dataTable.ajax.reload();
+                        } else {
+                            toastr["error"]("", "Coś poszło nie tak");
+                        }
+                    }
+                });
+            }
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swal.fire(
+                'Anulowano',
+                'Twoje dane są bezpieczne :)',
+                'error'
+            )
+        }
+    });
 }
